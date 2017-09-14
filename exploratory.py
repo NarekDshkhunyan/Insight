b574bd2754ab9fbe03b8bc5ee37ccba8bc5dd924
Narek <narek@mit.edu>
2017-09-13 17:26:11 -0700

    Better treatment of categorical variables in pandas

--- exploratory.py
+++ exploratory.py
@@ -13,8 +13,8 @@ qtype['fund_codes'] = qtype['fund_cc'].cat.codes
 
 
 # Plot the distribution of question tags
-tags = qtype['Categorial Type'].value_counts().keys()         # 58 unique tags
-values = qtype['Categorial Type'].value_counts().tolist()
+tags = qtype['qual_cc'].value_counts().keys()         # 58 unique tags
+values = qtype['qual_cc'].value_counts().tolist()
 
 plt.bar(np.arange(len(values)), values, align='center', alpha=0.5, color='blue')
 plt.xticks(np.arange(len(tags)), tags)
@@ -22,10 +22,20 @@ plt.ylabel('Number of tags')
 plt.title("Question tags")
 
 # Plot the distribution of fundamental questions
-tags = qtype['Fundamental Question'].value_counts().keys()     # 259 unique fundamental questions
-values = qtype['Fundamental Question'].value_counts().tolist()
+tags = qtype['fund_cc'].value_counts().keys()     # 259 unique fundamental questions
+values = qtype['fund_cc'].value_counts().tolist()
 
 plt.bar(np.arange(len(values)), values, align='center', alpha=0.5, color='blue')
 plt.xticks(np.arange(len(tags)), tags)
 plt.ylabel('Number of questions')
 plt.title("Fundamental questions")
+
+# Plot the distribution of question tags
+qtype = qtype.drop_duplicates() 
+tags = qtype['qual_cc'].value_counts().keys()         # 58 unique tags, most frequent : General - 425/2842
+values = qtype['qual_cc'].value_counts().tolist()
+
+plt.bar(np.arange(len(values)), values, align='center', alpha=0.5, color='blue')
+plt.xticks(np.arange(len(tags)), tags)
+plt.ylabel('Number of tags')
+plt.title("Question tags")
