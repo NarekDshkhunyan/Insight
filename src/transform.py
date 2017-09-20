57f3bfa628ce79f0d05858955d1ee6c05bc66749
Narek <narek@mit.edu>
2017-09-20 16:00:57 -0700

    Fixed the issue w/ randomized labels, updated gitignot

--- src/transform.py
+++ src/transform.py
@@ -11,12 +11,12 @@ def produceEmbeddings(type, data):
     tf_emb, tfidf_vectorizer = tfidf(data)
     cv_emb, count_vectorizer = cv(data)
 
-    #concat_emb = avg_feature_vector(sentences, 300)
+    concat_emb = avg_feature_vector(sentences, word2vec, 300)
 
     mean_emb = mean_embedding_vectorizer(sentences, word2vec, tfidf_vectorizer)
 
     if type == 'mean_emb': return mean_emb
-    #if type == 'concat_emb': return concat_emb
+    if type == 'concat_emb': return concat_emb
     if type == 'cv': return cv_emb
     if type == 'tfidf': return tf_emb
 
@@ -29,4 +29,4 @@ def getEmbeddings(data, labels, vocabulary_inv, type):
 
     # Get embeddings
     embeddings = produceEmbeddings(type, questions)
-    return embeddings
+    return embeddings, labels
