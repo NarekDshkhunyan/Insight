1bb36a0616a3174576707d1daae2ff2927f6fde2
Narek <narek@mit.edu>
2017-09-20 14:38:06 -0700

    Cleaned code for embeddings

--- src/embeddings.py
+++ src/embeddings.py
@@ -14,42 +14,20 @@ k = len(word2vec.itervalues().next())
 
 
 # -------------------------------------------------------------------------------------------------------------
-def cv(train_data, test_data):
+def cv(data):
     count_vectorizer = CountVectorizer()
 
-    train = count_vectorizer.fit_transform(train_data)
-    test = count_vectorizer.transform(test_data)
-    features = count_vectorizer.get_feature_names()
+    emb = count_vectorizer.fit_transform(data)
 
-    return train, test, count_vectorizer
+    return emb, count_vectorizer
 
 
-def tfidf(train_data, test_data):
+def tfidf(data):
     tfidf_vectorizer = TfidfVectorizer()
 
-    train = tfidf_vectorizer.fit_transform(train_data)
-    test = tfidf_vectorizer.transform(test_data)
-    features = tfidf_vectorizer.get_feature_names()
+    train = tfidf_vectorizer.fit_transform(data)
 
-    return train, test, tfidf_vectorizer
-
-
-def avg_feature_vector(sentences, model, NUM_FEATURES, k = 300):
-    """ Concatenates all words vector in a given sentence """
-
-    featureVec = np.zeros((len(sentences), NUM_FEATURES * k), dtype="float32")
-    for i in xrange(len(sentences)):
-        for j in xrange(len(sentences[i])):
-            sentence = sentences[i]
-            word = sentence[j]
-            if word in model:
-                featureVec[i][300*j:300*(j+1)] = model[word]
-            else:
-                #print word
-                featureVec[i][300*j:300*(j+1)] = np.random.uniform(-0.25, 0.25, k)
-        #featureVec[i] = np.divide(featureVec[i], len(sentences[i]))
-
-    return featureVec
+    return train, tfidf_vectorizer
 
 
 def mean_embedding_vectorizer(sentences, model, tfidf_vec):
@@ -57,12 +35,12 @@ def mean_embedding_vectorizer(sentences, model, tfidf_vec):
 
     max_idf = max(tfidf_vec.idf_)
     word2weight = defaultdict(lambda: max_idf, [(w, tfidf_vec.idf_[i]) for w, i in tfidf_vec.vocabulary_.items()])
-    return np.array([np.mean([model[w]*word2weight[w] for w in words if w in model] or [np.zeros(300)], axis=0) for words in sentences])
+    return np.array([np.mean([model[w] for w in words if w in model] or [np.zeros(300)], axis=0) for words in sentences]) # *word2weight[w]
+    #return np.stack([np.mean([model[word] for word in sentence if word in model] or [np.zeros(k)]) for sentence in sentences])
 
 
 def avg_feature_vector(sentences, model, k):
     """ Concatenates all words vector in a given sentence """
 
-    train = np.stack([np.stack([model[word] for word in sentence if word in model] or [np.zeros(k)]) for sentence in sentences])
-    return train
+    return np.stack([np.stack([model[word] if word in model else np.zeros(k) for word in sentence]) for sentence in sentences]) # np.random.uniform(-0.25, 0.25, k)
     #return np.array([np.concatenate([model[w] for w in words if w in model] or [np.zeros(k)], axis=0) for words in sentences])
--- src/transform.py
+++ src/transform.py
@@ -6,40 +6,33 @@ from sklearn.model_selection import KFold
 from utils import *
 
 # -------------------------------------------------------------------------------------------------------------
-def produceEmbeddings(type, train_data, test_data):
+def produceEmbeddings(type, data):
 
-    train_sentences = load_data(train_data)
-    test_sentences = load_data(test_data)
+    sentences = pad_sentences(load_data(data))
 
-    train_tf, test_tf, tfidf_vectorizer = tfidf(train_data, test_data)
-    train_cv, test_cv, count_vectorizer = cv(train_data, test_data)
+    tf_emb, tfidf_vectorizer = tfidf(data)
+    cv_emb, count_vectorizer = cv(data)
 
-    #train_embeddings = avg_feature_vector(train_sentences, word2vec, NUM_FEATURES, train, tfidf_vectorizer, 300)
-    #test_embeddings = avg_feature_vector(test_sentences, word2vec, NUM_FEATURES, test, tfidf_vectorizer, 300)
+    #concat_emb = avg_feature_vector(sentences, 300)
 
-    train_embeddings = mean_embedding_vectorizer(train_sentences, word2vec, tfidf_vectorizer)
-    test_embeddings = mean_embedding_vectorizer(test_sentences, word2vec, tfidf_vectorizer)
+    mean_emb = mean_embedding_vectorizer(sentences, word2vec, tfidf_vectorizer)
 
-    if type == 'mean_emb': return train_embeddings, test_embeddings
-    if type == 'concat_emb': return
-    if type == 'cv': return train_cv, test_cv
-    if type == 'tfidf': return train_tf, test_tf
+    if type == 'mean_emb': return mean_emb
+    #if type == 'concat_emb': return concat_emb
+    if type == 'cv': return cv_emb
+    if type == 'tfidf': return tf_emb
 
 # -------------------------------------------------------------------------------------------------------------
-data_file = "../Data/train_mat_filtered.pkl"
-#vocab_file = "../Data/vocab_filtered.pkl"
-vocab_inv_file = "../Data/vocab_inv_filtered.pkl"
+def getEmb(data, labels, vocabulary_inv):
+    data = clean_data(transform_data(vocabulary_inv, data))
+    questions, labels = get_random_samples(data, labels)
 
-with open(data_file) as f:
-    data, labels, _ = cPickle.load(f)
-with open(vocab_inv_file) as f:
-    vocabulary_inv = cPickle.load(f)
+    # Get embeddings
+    embeddings = produceEmbeddings('mean_emb', questions)
+    return embeddings
 
-data = clean_data(transform_data(vocabulary_inv, data))
-questions, labels = get_random_samples(data, labels)
 # Random split - think about KFold as well
-X_train, X_test, y_train, y_test = train_test_split(questions, labels, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.1, random_state=42)
 
-# Get embeddings
-X_train, X_test = produceEmbeddings('mean_emb', X_train, X_test)
+# Save the mebeddings
 cPickle.dump([X_train, X_test, y_train, y_test], open('../Data/mean_embeddings.pkl', 'wb'))
