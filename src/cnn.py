87527f5a9f610386c48dbdbf2ef82ded6243b8b1
Narek <narek@mit.edu>
2017-09-20 16:02:16 -0700

    Working CNN template

--- src/cnn.py
+++ src/cnn.py
@@ -1,23 +1,79 @@
 import numpy as np
+import cPickle
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Flatten, Input, MaxPooling1D, Convolution1D, Embedding
 from keras.layers.merge import Concatenate
-from keras.datasets import imdb
+#from keras.datasets import imdb
 from keras.preprocessing import sequence
 
 np.random.seed(0)
 
+#-------------------------------------------------------------------------------------------------------------
+from gensim.models import word2vec
+from os.path import join, exists, split
+import os
+import numpy as np
+
+def train_word2vec(sentence_matrix, vocabulary_inv,
+                   num_features=300, min_word_count=1, context=10):
+    """
+    Trains, saves, loads Word2Vec model
+    Returns initial weights for embedding layer.
+
+    inputs:
+    sentence_matrix # int matrix: num_sentences x max_sentence_len
+    vocabulary_inv  # dict {int: str}
+    num_features    # Word vector dimensionality
+    min_word_count  # Minimum word count
+    context         # Context window size
+    """
+    model_dir = 'models'
+    model_name = "{:d}features_{:d}minwords_{:d}context".format(num_features, min_word_count, context)
+    model_name = join(model_dir, model_name)
+    if exists(model_name):
+        embedding_model = word2vec.Word2Vec.load(model_name)
+        print('Load existing Word2Vec model \'%s\'' % split(model_name)[-1])
+    else:
+        # Set values for various parameters
+        num_workers = 2  # Number of threads to run in parallel
+        downsampling = 1e-3  # Downsample setting for frequent words
+
+        # Initialize and train the model
+        print('Training Word2Vec model...')
+        sentences = [[vocabulary_inv[w] for w in s] for s in sentence_matrix]
+        embedding_model = word2vec.Word2Vec(sentences, workers=num_workers,
+                                            size=num_features, min_count=min_word_count,
+                                            window=context, sample=downsampling)
+
+        # If we don't plan to train the model any further, calling
+        # init_sims will make the model much more memory-efficient.
+        embedding_model.init_sims(replace=True)
+
+        # Saving the model for later use. You can load it later using Word2Vec.load()
+        if not exists(model_dir):
+            os.mkdir(model_dir)
+        print('Saving Word2Vec model \'%s\'' % split(model_name)[-1])
+        embedding_model.save(model_name)
+
+    # add unknown words
+    embedding_weights = {key: embedding_model[word] if word in embedding_model else
+    np.random.uniform(-0.25, 0.25, embedding_model.vector_size)
+                         for key, word in vocabulary_inv.items()}
+
+
+    return embedding_weights
+
 # ---------------------- Parameters section -------------------
 #
 # Model type. See Kim Yoon's Convolutional Neural Networks for Sentence Classification, Section 3
-model_type = "CNN-non-static"  # CNN-rand|CNN-non-static|CNN-static
+model_type = "CNN-static"  # CNN-rand|CNN-non-static|CNN-static
 
 # Data source
 data_source = "keras_data_set"  # keras_data_set|local_dir
 
 # Model Hyperparameters
-embedding_dim = 50
+embedding_dim = 300
 filter_sizes = (3, 8)
 num_filters = 10
 dropout_prob = (0.5, 0.8)
@@ -71,7 +127,7 @@ def load_data(data_source):
 
 # Data Preparation
 print("Load data...")
-x_train, y_train, x_test, y_test, vocabulary_inv = load_data(data_source)
+#x_train, y_train, x_test, y_test, vocabulary_inv = load_data(data_source)
 
 if sequence_length != x_test.shape[1]:
     print("Adjusting sequence length for actual size")
@@ -91,11 +147,11 @@ if model_type in ["CNN-non-static", "CNN-static"]:
         x_test = np.stack([np.stack([embedding_weights[word] for word in sentence]) for sentence in x_test])
         print("x_train static shape:", x_train.shape)
         print("x_test static shape:", x_test.shape)
-
-elif model_type == "CNN-rand":
-    embedding_weights = None
-else:
-    raise ValueError("Unknown model type")
+#
+# elif model_type == "CNN-rand":
+#     embedding_weights = None
+# else:
+#     raise ValueError("Unknown model type")
 
 # Build model
 if model_type == "CNN-static":
@@ -140,6 +196,10 @@ if model_type == "CNN-non-static":
     embedding_layer = model.get_layer("embedding")
     embedding_layer.set_weights([weights])
 
+input_file = "../Data/mean_embeddings.pkl"
+with open(input_file) as f:
+    X_train, X_test, y_train, y_test = cPickle.load(f)
+
 # Train the model
-model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs,
-          validation_data=(x_test, y_test), verbose=2)
+model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs,
+          validation_data=(X_test, y_test), verbose=2)
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import KFold
 
 from transform import *
 from classify import *
+from cnn_core import *
 
 # -------------------------------------------------------------------------------------------------------------
 data_file = "../Data/train_mat_filtered.pkl"
@@ -16,17 +17,25 @@ with open(vocab_inv_file) as f:
     vocabulary_inv = cPickle.load(f)
 
 algorithm = 'svm'
-type = 'mean_emb'
+type = 'concat_emb'
 results = {'accuracy': [], 'precision': [], 'recall': [], 'f1': []}
 
 # -------------------------------------------------------------------------------------------------------------
-embeddings = getEmbeddings(data, labels, vocabulary_inv, type)
+embeddings, labels = getEmbeddings(data, labels, vocabulary_inv, type)
 # Random split
 X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.1, random_state=42)
-#cPickle.dump([X_train, X_test, y_train, y_test], open('../Data/mean_embeddings.pkl', 'wb'))
+print X_train.shape, X_test.shape
+cPickle.dump([X_train, X_test, y_train, y_test], open('../Data/concat_embeddings.pkl', 'wb'))
 
 # Run the classification algorithm
-classify(algorithm, X_train, y_train, X_test, y_test, results)
+#classify(algorithm, X_train, y_train, X_test, y_test, results)
+
+model = CNN()
+model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs,
+          validation_data=(X_test, y_test), verbose=2)
+predictions_valid = model.predict(X_test, batch_size=batch_size, verbose=1)
+score = log_loss(y_test, predictions_valid)
+print('Score log_loss: ', score)
 
 # Cross-validation
 #for k, (train, test) in enumerate(KFold(10).split(embeddings, labels)):
